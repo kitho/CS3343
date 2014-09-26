cf919f99693baf9a636a5fcc8c69f67ee16f3e83
kitho <kithopersonal@gmail.com>
2014-09-26 22:06:15 +0800

    Project init

---
+++ src/main/java/CS3343/AirlineTicketOrdering/App.java
@@ -0,0 +1,13 @@
+package CS3343.AirlineTicketOrdering;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
