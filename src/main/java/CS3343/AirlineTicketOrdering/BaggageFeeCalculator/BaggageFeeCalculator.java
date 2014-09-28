3192844075bb431725b43b169490b2d70e4ccef6
Nelson <nelsonwongnormal@gmail.com>
2014-09-29 01:52:21 +0800

    BaggageFeeCal

---
+++ src/main/java/CS3343/AirlineTicketOrdering/BaggageFeeCalculator/BaggageFeeCalculator.java
@@ -0,0 +1,27 @@
+package CS3343.AirlineTicketOrdering.BaggageFeeCalculator;
+
+import CS3343.AirlineTicketOrdering.Model.Flight;
+
+public class BaggageFeeCalculator {
+	private Flight flight;
+	
+	public BaggageFeeCalculator (Flight flight){
+		
+	}
+	
+	public float calFee(float baggageWeight, String travelClass){
+		float baggageFee;
+		
+		//float maxBaggageWeight = flight.getMaxBaggageWeight(travelClass);
+		float maxBaggageWeight = 10;
+		
+		if(baggageWeight < maxBaggageWeight){
+			baggageFee = 0;
+		}else{
+			
+		}
+		return 0;
+	}
+	
+	
+}
