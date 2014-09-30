2561b1c07ccdd7c098e818a192ad94bdabbc5a7c
kitho <kithopersonal@gmail.com>
2014-09-30 21:33:17 +0800

    New Unit Test: Airline Query Test

---
+++ src/test/java/CS3343/AirlineTicketOrdering/DataQuery/AirlineQueryTest.java
@@ -0,0 +1,14 @@
+package CS3343.AirlineTicketOrdering.DataQuery;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AirlineQueryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
