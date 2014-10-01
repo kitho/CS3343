245886070fa0888cfc309b995d0271d08bce9169
kitho <kithopersonal@gmail.com>
2014-10-01 23:49:55 +0800

    New Unit Test Cases: Airline Query Test

--- src/test/java/CS3343/AirlineTicketOrdering/DataQuery/AirlineQueryTest.java
+++ src/test/java/CS3343/AirlineTicketOrdering/DataQuery/AirlineQueryTest.java
@@ -30,6 +30,39 @@ public class AirlineQueryTest {
 	}
 
 	@Test
+	public void findFlightsByDepatureAndDestinationAndDateWithoutSuitableFlightTest() throws ParseException, IOException {
+		SourceReader<AirlineCompany> airlineCompanyReader = mock(AirlineCompanyCSVFileReader.class);
+		
+		String depature = "Japan";
+		String destination = "Australia"; 
+		Date depatureDate = formatter.parse("2014-12-31 00:00:00");
+		
+		Flight flight = new Flight();
+		flight.setAirline("Cathay Pacific Airways");
+		flight.setFlightNumber("CP001");
+		flight.setTravelClass("FIRST");
+		flight.setDepature("Hong Kong");
+		flight.setDestination("Taiwan");
+		flight.setDepatureDateTime(formatter.parse("2014-01-01 14:30:00"));
+		flight.setArrivalDateTime(formatter.parse("2014-01-01 17:30:00"));
+		flight.setAvailable(30);
+		flight.setOneWayPrice(2500.00);
+		
+		AirlineCompany airlineCompany = new AirlineCompany("Cathay Pacific Airways");
+		airlineCompany.addFlight(flight);
+		
+		List<AirlineCompany> airlineCompanies= new ArrayList<AirlineCompany>();
+		airlineCompanies.add(airlineCompany);
+		
+		when(airlineCompanyReader.read()).thenReturn(airlineCompanies);
+		
+		AirlineQuery airlineQuery = new AirlineQuery(airlineCompanyReader);
+		List<Flight> flights = airlineQuery.findFlightsByDepatureAndDestinationAndDate(depature, destination, depatureDate);
+		
+		assertThat(0, is(flights.size()));
+	}
+	
+	@Test
 	public void findFlightsByDepatureAndDestinationAndDateForSingleAirlineAndSingleFlightTest() throws ParseException, IOException {
 		SourceReader<AirlineCompany> airlineCompanyReader = mock(AirlineCompanyCSVFileReader.class);
 		
@@ -58,6 +91,8 @@ public class AirlineQueryTest {
 		
 		AirlineQuery airlineQuery = new AirlineQuery(airlineCompanyReader);
 		List<Flight> flights = airlineQuery.findFlightsByDepatureAndDestinationAndDate(depature, destination, depatureDate);
+		
+		assertThat(1, is(flights.size()));
 		
 		Flight flightResult = flights.get(0);
 		
@@ -133,6 +168,95 @@ public class AirlineQueryTest {
 		
 		List<AirlineCompany> airlineCompanies= new ArrayList<AirlineCompany>();
 		airlineCompanies.add(airlineCompany);
+		
+		when(airlineCompanyReader.read()).thenReturn(airlineCompanies);
+		
+		AirlineQuery airlineQuery = new AirlineQuery(airlineCompanyReader);
+		List<Flight> flightsResult = airlineQuery.findFlightsByDepatureAndDestinationAndDate(depature, destination, depatureDate);
+		
+		assertThat(flights.size(), is(flightsResult.size()));
+		
+		for (Flight flightResult : flightsResult) {
+			assertThat(depature, is(flightResult.getDepature()));
+			assertThat(destination, is(flightResult.getDestination()));
+			assertThat(true, is(DateUtils.isSameDay(depatureDate, flightResult.getDepatureDateTime())));
+		}
+		
+		for (int i = 0; i < flights.size(); i++) {
+			assertThat(flights.get(i).getAirline(), is(flightsResult.get(i).getAirline()));
+			assertThat(flights.get(i).getFlightNumber(), is(flightsResult.get(i).getFlightNumber()));
+			assertThat(flights.get(i).getTravelClass(), is(flightsResult.get(i).getTravelClass()));
+			assertThat(flights.get(i).getDepature(), is(flightsResult.get(i).getDepature()));
+			assertThat(flights.get(i).getDestination(), is(flightsResult.get(i).getDestination()));
+			assertThat(flights.get(i).getDepatureDateTime(), is(flightsResult.get(i).getDepatureDateTime()));
+			assertThat(flights.get(i).getArrivalDateTime(), is(flightsResult.get(i).getArrivalDateTime()));
+			assertThat(flights.get(i).getAvailable(), is(flightsResult.get(i).getAvailable()));
+			assertThat(flights.get(i).getOneWayPrice(), is(flightsResult.get(i).getOneWayPrice()));
+		}
+
+		
+	}
+	
+	@Test
+	public void findFlightsByDepatureAndDestinationAndDateForMultipleAirlinesAndMultipleFlightsTest() throws ParseException, IOException {
+		SourceReader<AirlineCompany> airlineCompanyReader = mock(AirlineCompanyCSVFileReader.class);
+		
+		String depature = "Hong Kong";
+		String destination = "Taiwan"; 
+		Date depatureDate = formatter.parse("2014-01-01 00:00:00");
+		
+		Flight flight1 = new Flight();
+		
+		flight1.setAirline("Cathay Pacific Airways");
+		flight1.setFlightNumber("CP001");
+		flight1.setTravelClass("FIRST");
+		flight1.setDepature(depature);
+		flight1.setDestination(destination);
+		flight1.setDepatureDateTime(formatter.parse("2014-01-01 09:30:00"));
+		flight1.setArrivalDateTime(formatter.parse("2014-01-01 11:30:00"));
+		flight1.setAvailable(30);
+		flight1.setOneWayPrice(2500.00);
+		
+		Flight flight2 = new Flight();
+		
+		flight2.setAirline("China Airlines");
+		flight2.setFlightNumber("CA001");
+		flight2.setTravelClass("FIRST");
+		flight2.setDepature(depature);
+		flight2.setDestination(destination);
+		flight2.setDepatureDateTime(formatter.parse("2014-01-01 12:30:00"));
+		flight2.setArrivalDateTime(formatter.parse("2014-01-01 14:30:00"));
+		flight2.setAvailable(30);
+		flight2.setOneWayPrice(2500.00);
+		
+		Flight flight3 = new Flight();
+		
+		flight3.setAirline("Hong Kong Airlines");
+		flight3.setFlightNumber("HKA001");
+		flight3.setTravelClass("FIRST");
+		flight3.setDepature(depature);
+		flight3.setDestination(destination);
+		flight3.setDepatureDateTime(formatter.parse("2014-01-01 19:30:00"));
+		flight3.setArrivalDateTime(formatter.parse("2014-01-01 21:30:00"));
+		flight3.setAvailable(30);
+		flight3.setOneWayPrice(2500.00);
+		
+		List<Flight> flights = new ArrayList<Flight>();
+		flights.add(flight1);
+		flights.add(flight2);
+		flights.add(flight3);
+		
+		AirlineCompany airlineCompany1 = new AirlineCompany("Cathay Pacific Airways");
+		airlineCompany1.addFlight(flight1);
+		AirlineCompany airlineCompany2 = new AirlineCompany("China Airlines");
+		airlineCompany2.addFlight(flight2);
+		AirlineCompany airlineCompany3 = new AirlineCompany("Hong Kong Airlines");
+		airlineCompany3.addFlight(flight3);
+		
+		List<AirlineCompany> airlineCompanies= new ArrayList<AirlineCompany>();
+		airlineCompanies.add(airlineCompany1);
+		airlineCompanies.add(airlineCompany2);
+		airlineCompanies.add(airlineCompany3);
 		
 		when(airlineCompanyReader.read()).thenReturn(airlineCompanies);
 		
