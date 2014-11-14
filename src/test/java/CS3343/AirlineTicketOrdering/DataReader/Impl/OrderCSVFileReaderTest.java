bdbbe9641cabadc03bc71b4e1a483cd26e7bf41f
Leo <leoyyy@users.noreply.github.com>
2014-11-15 01:01:08 +0800

    add OrderCSV

---
+++ src/test/java/CS3343/AirlineTicketOrdering/DataReader/Impl/OrderCSVFileReaderTest.java
@@ -0,0 +1,103 @@
+package CS3343.AirlineTicketOrdering.DataReader.Impl;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import TestingTool.DataWriter.AirlineCompanyCSVFileWriter;
+import CS3343.AirlineTicketOrdering.CSVFile.CSVFile;
+import CS3343.AirlineTicketOrdering.CustomDateUtil.CustomDateFormatter;
+import CS3343.AirlineTicketOrdering.DataReader.SourceReader;
+import CS3343.AirlineTicketOrdering.DataReader.Impl.AirlineCompanyCSVFileReader;
+import CS3343.AirlineTicketOrdering.DataWriter.SourceWriter;
+import CS3343.AirlineTicketOrdering.DataWriter.Impl.OrderCSVFileWriter;
+import CS3343.AirlineTicketOrdering.Model.AirlineCompany;
+import CS3343.AirlineTicketOrdering.Model.Flight;
+import CS3343.AirlineTicketOrdering.Model.Order;
+import CS3343.AirlineTicketOrdering.Parser.Impl.AirlineCompanyParser;
+import CS3343.AirlineTicketOrdering.Parser.Impl.OrderParser;
+
+public class OrderCSVFileReaderTest {
+
+	private File projectPath;
+	private CustomDateFormatter formatter;
+	
+	@Before
+	public void setUp() throws IOException{
+		projectPath = new File(".").getCanonicalFile(); 
+		formatter = new CustomDateFormatter();
+
+	}
+	
+	@Test
+	public void readOrderCSVFileWhenFileNotExisted() throws IOException{
+		Files.deleteIfExists(Paths.get(projectPath + CSVFile.ORDERCSV.value()));
+		
+		SourceReader<Order> orderCsvReader;
+		try {
+			orderCsvReader = new OrderCSVFileReader(projectPath + CSVFile.ORDERCSV.value());
+			fail("File not existed");
+		} catch (FileNotFoundException e) {
+			assertThat(e.getMessage().toString(), is(not(nullValue())));
+		}
+
+	}
+
+	@Test
+	public void readOrderCSVFileWithOneRecordTest() throws IOException, ParseException{
+		Files.deleteIfExists(Paths.get(projectPath + CSVFile.ORDERCSV.value()));
+		
+		Flight flight = new Flight();
+		flight.setAirline("Cathay Pacific Airways");
+		flight.setFlightNumber("CP001");
+		flight.setTravelClass("FIRST");
+		flight.setDepature("Hong Kong");
+		flight.setDestination("Taiwan");
+		flight.setDepatureDateTime(formatter.parse("2014-01-01 14:30:00"));
+		flight.setArrivalDateTime(formatter.parse("2014-01-01 17:30:00"));
+		flight.setAvailable(30);
+		flight.setOneWayPrice(2500.00);
+
+		Order order = new Order();
+		order.setId(0);
+		order.setFlight(flight);
+		order.setNumberOfTicket(1);
+		
+		ArrayList<Order> orders = new ArrayList<Order>();
+		orders.add(order);
+		
+		SourceWriter<List<Order>> orderSourceWriter = new OrderCSVFileWriter(projectPath + CSVFile.ORDERCSV.value());
+		orderSourceWriter.write(orders);
+		orderSourceWriter.close();
+		
+		SourceReader<Order> orderReader = new OrderCSVFileReader(projectPath + CSVFile.ORDERCSV.value());
+		List<Order> orderResult = orderReader.read(new OrderParser());
+		orderReader.close();
+		
+		assertThat(order.getId(), is(orderResult.get(0).getId()));
+		assertThat(order.getFlight(), is(orderResult.get(0).getFlight()));
+	}
+	
+	
+	@Test
+	public void readOrderCSVFileWithThreeRecordTest() throws IOException, ParseException{		
+
+		
+	}
+	
+	
+}
--- src/test/java/CS3343/AirlineTicketOrdering/FlightPathFinding/PathFindingTest.java
+++ src/test/java/CS3343/AirlineTicketOrdering/FlightPathFinding/PathFindingTest.java
@@ -3,20 +3,14 @@ package CS3343.AirlineTicketOrdering.FlightPathFinding;
 import java.util.ArrayList;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.fail;
 
 
 
-import org.junit.Before;
 import org.junit.Test;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 import CS3343.AirlineTicketOrdering.Model.Route;
-import CS3343.AirlineTicketOrdering.FlightPathFinding.*;
 
 
 public class PathFindingTest {
