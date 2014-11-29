2e3e41d45828919299ff9a7b8516a27fca22c7e7
kamyulau <yu1577@hotmail.com>
2014-11-30 02:14:08 +0800

    Update test case

--- src/test/java/CS3343/AirlineTicketOrdering/FlyerMiles/FlyerMilesTest.java
+++ src/test/java/CS3343/AirlineTicketOrdering/FlyerMiles/FlyerMilesTest.java
@@ -2,11 +2,15 @@ package CS3343.AirlineTicketOrdering.FlyerMiles;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import CS3343.AirlineTicketOrdering.FlightPathFinding.PathFinding;
 import CS3343.AirlineTicketOrdering.FlightPathFinding.RouteTable;
 import CS3343.AirlineTicketOrdering.Model.CreditCard;
+import CS3343.AirlineTicketOrdering.Model.Flight;
 import CS3343.AirlineTicketOrdering.Model.FlightClass;
 import CS3343.AirlineTicketOrdering.Model.Route;
 
@@ -17,20 +21,80 @@ public class FlyerMilesTest {
 		CreditCard card = new CreditCard();
 		card.setBank("HSBC");
 		card.setCreditCardType("VISA");
-		
+		ArrayList<Route> routeList = new ArrayList<Route>();
+		ArrayList<Flight> flight = new ArrayList<Flight>();
 		FlyerMiles fm = new FlyerMiles();
 		
-		String from = "Hong Kong";
-		String to = "Thailand";
-		RouteTable routeTable = new RouteTable();
-		PathFinding	pathFinding = new PathFinding(from, to, new RouteTable());
-		Route r = pathFinding.findRoute(from, to, routeTable.getRouteList());
+		Route r = new Route();
+		r.setDeparture("Hong Kong");
+		r.setDestination("Taiwan");
+		r.setDistance(800);
+		routeList.add(r);
 		
-		int result1 = fm.awareMiles(r, FlightClass.FIRST_CLASS , card);
+		Flight f = new Flight();
+		f.setAirline("Cathay Pacific Airways");
+		f.setTravelClass("");
+		flight.add(f);
 		
-		int expect1 = (int)(2500 * (1+0.1+0.4));
+		int result1 = fm.awardMiles(routeList, flight, card);
+		
+		int expect1 = (int)(1500 * (1+0.1+0.1+0.0));
 		System.out.println(result1);
-		assertEquals(result1,0);
+		assertEquals(result1,expect1);
+	}
+	
+	@Test
+	public void test02() {
+		CreditCard card = new CreditCard();
+		card.setBank("ABC");
+		card.setCreditCardType("VISA");
+		ArrayList<Route> routeList = new ArrayList<Route>();
+		ArrayList<Flight> flight = new ArrayList<Flight>();
+		FlyerMiles fm = new FlyerMiles();
+		
+		Route r = new Route();
+		r.setDeparture("Hong Kong");
+		r.setDestination("Taiwan");
+		r.setDistance(800);
+		routeList.add(r);
+		
+		Flight f = new Flight();
+		f.setAirline("TEST");
+		f.setTravelClass("");
+		flight.add(f);
+		
+		int result1 = fm.awardMiles(routeList, flight, card);
+		
+		int expect1 = (int)(1500 * (1+0.0+0.0+0.0));
+		System.out.println(result1);
+		assertEquals(result1,expect1);
+	}
+	
+	@Test
+	public void test03() {
+		CreditCard card = new CreditCard();
+		card.setBank("DBS");
+		card.setCreditCardType("VISA");
+		ArrayList<Route> routeList = new ArrayList<Route>();
+		ArrayList<Flight> flight = new ArrayList<Flight>();
+		FlyerMiles fm = new FlyerMiles();
+		
+		Route r = new Route();
+		r.setDeparture("Hong Kong");
+		r.setDestination("Taiwan");
+		r.setDistance(5000);
+		routeList.add(r);
+		
+		Flight f = new Flight();
+		f.setAirline("Dragonair");
+		f.setTravelClass(FlightClass.FIRST_CLASS);
+		flight.add(f);
+		
+		int result1 = fm.awardMiles(routeList, flight, card);
+		
+		int expect1 = (int)(2500 * (1+0.09+0.05+0.4));
+		System.out.println(result1);
+		assertEquals(result1,expect1);
 	}
 
 }
