df6ee323892cef8dbfd3ca037e38005b6556deb9
kamyulau <yu1577@hotmail.com>
2014-11-29 23:27:00 +0800

    Update new version

--- src/main/java/CS3343/AirlineTicketOrdering/FlyerMiles/FlyerMiles.java
+++ src/main/java/CS3343/AirlineTicketOrdering/FlyerMiles/FlyerMiles.java
@@ -1,5 +1,8 @@
 package CS3343.AirlineTicketOrdering.FlyerMiles;
 
+import java.util.List;
+
+import CS3343.AirlineTicketOrdering.FlightPathFinding.FlightPath;
 import CS3343.AirlineTicketOrdering.Model.*;
 
 public class FlyerMiles {
@@ -11,14 +14,29 @@ public class FlyerMiles {
 	//calculate the aware miles points
 	public int awareMiles(Route route, String FlightClass, CreditCard card){
 		if(route !=null){
-			int point = 0;
+			/*int point = 0;
 			MilesCalculator mc = new MilesCalculator();
 			BonusChecker bc = new BonusChecker(card,FlightClass);
 			point = mc.findBasePoints(route.getDistance());
 			point = (int) (point * (1 + bc.getCreditCardBounsRate() + bc.getFlightClassBounsRate()));
 			
-			return point;
+			return point;*/
 		}
 		return 0;
+	}
+	
+	public int awardMiles(FlightPath fPath, List<Flight> flight, CreditCard card){
+		int point = 0;
+		MilesCalculator mc = new MilesCalculator();
+		for (int i = 0; i < fPath.getFlightList().size(); i++){
+			int route_point = 0;
+			Route route = fPath.getFlightList().get(i);
+			BonusChecker bc = new BonusChecker(card,flight.get(i).getTravelClass(),flight.get(i).getAirline());
+			route_point = mc.findBasePoints(route.getDistance());
+			route_point = (int) (route_point * (1 + bc.getCreditCardBounsRate() + bc.getFlightClassBounsRate() + bc.getCompanyBounsRate()));
+			point += route_point;
+			
+		}
+		return point;
 	}
 }
